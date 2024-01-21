6e8de73ffee588f28787de17776d7348789df373
victor.gomes <vitinhoxd6@hotmail.com>
2024-01-20 23:08:50 -0300

    new ignore

--- install.cmd
+++ install.cmd
@@ -1,5 +1,8 @@
 :: call conda env list
 CALL conda env list > checks/output/available_envs.txt
 
+:: waits 4 seconds
+PING 127.0.0.1 -n 5 > nul
+
 :: makes necessary checks for the dashboard initialization
 python checks/checks_and_installs_env.py
